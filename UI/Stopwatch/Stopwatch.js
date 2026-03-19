bb035c3c23c56e672f0c3d4ab19424e2d3144135
Elvin Musayev <msayev02@gmail.com>
2026-03-19 16:05:44 +0400

    Refactor: Enhance Stopwatch component with improved time formatting and announcement features

--- UI/Stopwatch/Stopwatch.js
+++ UI/Stopwatch/Stopwatch.js
@@ -7,21 +7,34 @@ function Button({ variant = "vanilla", children, ...props }) {
   );
 }
 
+function formatTimeSection(section) {
+  return section.toString().padStart(2, "0");
+}
+
 export default function Stopwatch() {
   const [isGoing, setIsGoing] = useState(false);
   const [diff, setDiff] = useState(0);
+
+  const [announcement, setAnnouncement] = useState("");
   const startTime = useRef(0);
+
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (60 * 1000));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  let ms = Math.floor((diff % 1000) / 10);
+  const time = `${formatTimeSection(minutes)}:${formatTimeSection(seconds)}:${formatTimeSection(ms)}`;
 
   function handleStart() {
     if (!isGoing) {
       startTime.current = Date.now() - diff;
     }
+    setAnnouncement(isGoing ? `stopped at ${time}` : "stopwatch started");
     setIsGoing((prev) => !prev);
   }
 
   function handleReset() {
     setIsGoing(false);
     setDiff(0);
+    setAnnouncement("stopwatch reset");
   }
 
   useEffect(() => {
@@ -32,13 +45,6 @@ export default function Stopwatch() {
     return () => clearInterval(intervalID);
   }, [isGoing]);
 
-  function display() {
-    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (60 * 1000));
-    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-    let ms = Math.floor((diff % 1000) / 10);
-    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${ms.toString().padStart(2, "0")}`;
-  }
-
   return (
     <div className="stopwatch">
       <div className="display-wrapper">
@@ -47,18 +53,20 @@ export default function Stopwatch() {
           className={`stopwatch__display ${isGoing ? "stopwatch__display--animate" : ""}`}
         ></div>
         <button
+          id="display"
           onClick={handleStart}
           aria-label={isGoing ? "stop the stopwatch" : "start the stopwatch"}
         >
-          {display()}
+          {time}
         </button>
+        {/* to announce screen readers the time only on stop or start or reset
+         as they don't need to get the update for every millisecond */}
+        <div aria-live="polite" aria-atomic="true" className="visually-hidden">
+          {announcement}
+        </div>
       </div>
       <div className="controls">
-        <Button
-          onClick={handleStart}
-          aria-controls="display"
-          aria-live="polite"
-        >
+        <Button onClick={handleStart} aria-controls="display">
           {isGoing ? "Stop" : "Start"}
         </Button>
         <Button
